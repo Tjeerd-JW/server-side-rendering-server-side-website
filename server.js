b8586c6491a945131a892acb1698486aac358d0f
Tjeerd <tj.wokke@gmail.com>
2026-03-03 14:47:03 +0100

    initial commit

--- server.js
+++ server.js
@@ -60,3 +60,12 @@ app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+
+app.get('/talent-awards', async function (request, response) {
+  response.render('talent-awards.liquid')
+})
+
+app.get('/talent-awards/:name', async function (request, response) {
+  response.render('talent-candidate.liquid')
+})
